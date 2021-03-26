4f8e4d6830c5dfe0618942cd6e83c5a1b8a04956
Rafał Korzeniec <51083650+RafKorz@users.noreply.github.com>
2021-03-26 23:58:33 +0100

    Update Tests.java

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -3,6 +3,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,7 +14,7 @@ public class Tests
     @Before
     public void setUp()
     {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Rafal\\Desktop\\ChromeDriver\\Nowy folder\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Rafal\\Desktop\\ChromeDriver\\ChromeDriver89\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.navigate().to("http://www.wikipedia.pl");
     }
@@ -22,8 +23,27 @@ public class Tests
     public void wikiTest()
     {
         Assert.assertEquals(driver.getTitle(), "Wikipedia, wolna encyklopedia");
-        driver.findElement(By.id("searchInput")).sendKeys("Częstochowa");
+        driver.findElement(By.cssSelector("#searchInput")).sendKeys("Częstochowa");
         driver.findElement(By.id("searchButton")).click();
+        Assert.assertEquals(true, verifyElementPresent(driver, By.id("Nazwa")));
+        Assert.assertEquals(false, verifyElementPresent(driver, By.id("qqqq")));
+        verifyElementPresent(driver, By.id("Historia"));
+        verifyElementPresent(driver, By.id("qqqqqqqq"));
+        driver.findElement(By.linkText("Historia Częstochowy")).click();
+        Assert.assertEquals(driver.getTitle(), "Historia Częstochowy – Wikipedia, wolna encyklopedia");
+    }
+
+    public static boolean verifyElementPresent(WebDriver driver, By by)
+    {
+        try{
+            driver.findElement(by);
+            System.out.println("Element found");
+            return true;
+        }
+        catch (NoSuchElementException e){
+            System.out.println("Element not found");
+            return false;
+        }
     }
 
     @After
